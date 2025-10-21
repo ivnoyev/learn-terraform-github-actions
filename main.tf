b5741467cf84f67dc5fa5e6108930b0271030c4d
Ivan Noev <Ivan.Noev@madnesspartners.com>
2025-10-22 04:20:48 +0500

    more fixes

--- main.tf
+++ main.tf
@@ -37,18 +37,6 @@ resource "aws_security_group" "jmeter_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 8086
-    to_port     = 8086
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -93,23 +81,5 @@ resource "aws_instance" "jmeter_slave" {
   tags                   = { Name = "JMeter-Slave-${count.index}" }
 }
 
-resource "aws_instance" "monitoring" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.jmeter_sg.id]
-  key_name               = "jmeter-key"
-  user_data              = <<-EOF
-    #!/bin/bash
-    apt-get update -y
-    apt-get install -y docker.io
-    docker run -d -p 8086:8086 --name influxdb influxdb:2.7
-    docker exec influxdb influx setup --org jmeter-org --bucket jmeter --username admin --password admin1234 --token my-token --force
-    docker run -d -p 3000:3000 --name grafana grafana/grafana
-    EOF
-  tags                   = { Name = "Monitoring" }
-}
-
 output "master_ip" { value = aws_instance.jmeter_master.public_ip }
 output "slave_ip" { value = aws_instance.jmeter_slave[0].private_ip }
-output "monitoring_ip" { value = aws_instance.monitoring.public_ip }
-output "monitoring_private_ip" { value = aws_instance.monitoring.private_ip }
