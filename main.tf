87c84a33aba5fb5fb01aec3e7fe033a3d889c2b3
Ivan Noev <Ivan.Noev@madnesspartners.com>
2025-10-21 22:36:14 +0500

    merged 2 repos

--- main.tf
+++ main.tf
@@ -1,74 +1,45 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
 resource "random_pet" "sg" {}
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+# VPC
+resource "aws_vpc" "jmeter_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags                 = { Name = "jmeter-vpc" }
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+resource "aws_subnet" "jmeter_subnet" {
+  vpc_id     = aws_vpc.jmeter_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags       = { Name = "jmeter-subnet" }
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
+# JMeter Security Group (RMI: 1099, 50000; SSH: 22)
+resource "aws_security_group" "jmeter_sg" {
+  name   = "${random_pet.sg.id}-sg"
+  vpc_id = aws_vpc.jmeter_vpc.id
+
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 1099
+    to_port     = 1099
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
     to_port     = 0
@@ -77,6 +48,88 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+# Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+# JMeter Master
+resource "aws_instance" "jmeter_master" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.small"
+  subnet_id              = aws_subnet.jmeter_subnet.id
+  vpc_security_group_ids = [aws_security_group.jmeter_sg.id]
+  key_name               = "jmeter-ssh-key"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y openjdk-11-jdk
+    wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.6.3.tgz
+    tar -xzf apache-jmeter-5.6.3.tgz -C /opt
+    ln -s /opt/apache-jmeter-5.6.3 /opt/jmeter
+    echo "server.rmi.ssl.disable=true" >> /opt/jmeter/bin/jmeter.properties
+    EOF
+
+  tags = { Name = "JMeter-Master" }
+}
+
+# JMeter Slaves
+variable "slave_count" { default = 2 }
+
+resource "aws_instance" "jmeter_slave" {
+  count                  = var.slave_count
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.jmeter_subnet.id
+  vpc_security_group_ids = [aws_security_group.jmeter_sg.id]
+  key_name               = "jmeter-ssh-key"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y openjdk-11-jdk
+    wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.6.3.tgz
+    tar -xzf apache-jmeter-5.6.3.tgz -C /opt
+    ln -s /opt/apache-jmeter-5.6.3 /opt/jmeter
+    echo "server.rmi.ssl.disable=true" >> /opt/jmeter/bin/jmeter.properties
+    /opt/jmeter/bin/jmeter-server &
+    EOF
+
+  tags = { Name = "JMeter-Slave-${count.index}" }
+}
+
+# Monitoring
+resource "aws_instance" "monitoring" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.jmeter_subnet.id
+  vpc_security_group_ids = [aws_security_group.jmeter_sg.id]
+  key_name               = "jmeter-ssh-key"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io
+    docker run -d -p 8086:8086 influxdb:2.7
+    docker run -d -p 3000:3000 grafana/grafana
+    EOF
+
+  tags = { Name = "Monitoring" }
+}
+
+# IP addresses to output
+output "master_ip" {
+  value = aws_instance.jmeter_master.public_ip
+}
+output "slave_ips" {
+  value = join(",", aws_instance.jmeter_slave[*].private_ip)
+}
+output "grafana_url" {
+  value = "http://${aws_instance.monitoring.public_ip}:3000"
 }
